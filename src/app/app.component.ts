dbe6270a0547b4bb263d59ebca01064b4c5eda60
xcfw <dr@gorsvet.kz>
2019-03-14 09:13:35 +0000

    addings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular7-router-demo';
+  title = 'angular7 webdev lectures demo';
 }
